e1c3e6fea0d78b75d28a45001a6b94b947cbe461
Nickson4k <nickson4k@gmail.com>
2026-06-09 21:18:48 +0300

    api

--- api/forecast.js
+++ api/forecast.js
@@ -30,7 +30,7 @@ module.exports = async (req, res) => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model: 'meta-llama/llama-3.3-70b-instruct:free',
+        model: 'openrouter/free',
         messages: [
           {
             role: 'system',
